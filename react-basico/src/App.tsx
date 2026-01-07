51f81bff111f7c024ea39421837dd53ab4bae9ff
Jadson <jadsontavarests@hotmail.com>
2026-01-06 23:01:15 -0300

    aula 03

--- react-basico/src/App.tsx
+++ react-basico/src/App.tsx
@@ -1,15 +1,8 @@
-import { useState } from "react";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
+export function App() {
   return (
     <>
       <p>hello wordl</p>
-
-      <button onClick={() => setCount(count + 1)}>count is: {count}</button>
     </>
   );
 }
 
-export default App;
